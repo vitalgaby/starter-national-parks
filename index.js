628e18596be56c650482abe524662f5de23cf99a
Vitalgaby <gabsanchez18@outlook.com>
2024-02-07 05:54:18 -0800

    initial

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 // const firstBtn = document.querySelector("button");
 
 // firstBtn.addEventListener("click", function (event) {
@@ -109,4 +110,47 @@ const favoriteButtonClickHandler = (event) => {
   
   // Add event listener for DOMContentLoaded
   window.addEventListener("DOMContentLoaded", main);
+
+
+
+
+
   
+// If there are no errors
+if (!Object.keys(errors).length) {
+    // Create a new element
+    const parkSection = document.createElement("section");
+  
+    // Add the park class
+    parkSection.classList.add("park-display");
+  
+    // Construct the HTML for this element
+    const content = `
+      <h2>${formData.get("name")}</h2>
+      <div class="location-display">${formData.get("location")}</div>
+      <div class="description-display">${formData.get("description")}</div>
+      <button class="rate-button" title="Add to Favourites">&#9734;</button>
+      <div class="stats">
+        <div class="established-display stat">
+          <h3>Established</h3>
+          <div class="value">${moment(formData.get("established")).format(
+            "MMMM D, YYYY"
+          )}</div>
+        </div>
+        <div class="area-display stat">
+          <h3>Area</h3>
+          <div class="value">${formData.get("area")}</div>
+        </div>
+        <div class="rating-display stat">
+          <h3>Rating</h3>
+          <div class="value">${formData.get("rating")}</div>
+        </div>
+      </div>
+      `;
+  
+    // Set the innerHTML
+    parkSection.innerHTML = content;
+  
+    // Append to the main element
+    document.querySelector("main").appendChild(parkSection);
+  }
